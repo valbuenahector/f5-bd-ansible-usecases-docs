764b41b2a9695be054b1cd36055c1ca8f0a999b2
vtog <v.tognaci@f5.com>
2020-10-27 15:49:28 -0500

    should have added conf.py to last commit

--- docs/conf.py
+++ docs/conf.py
@@ -187,7 +187,7 @@ html_short_title = 'Container Ingress Services Home'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = '_static/f5-logo-solid-rgb_small.png'
+html_logo = '_static/f5-logo-rgb.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or
