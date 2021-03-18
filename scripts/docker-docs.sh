0f98db1f4b04028663dfcff12e8ef60da8d39847
vtog <v.tognaci@f5.com>
2021-03-18 15:20:51 -0500

    fixing build script and rst warnings

--- scripts/docker-docs.sh
+++ scripts/docker-docs.sh
@@ -2,10 +2,12 @@
 
 set -x
 
+COMMAND="make html"
+
 : ${DOC_IMG:=f5devcentral/containthedocs:latest}
 
 exec docker run --rm -it \
   -v $PWD:$PWD --workdir $PWD \
   ${DOCKER_RUN_ARGS} \
   -e "LOCAL_USER_ID=$(id -u)" \
-  ${DOC_IMG} "$@"
+  ${DOC_IMG} ${COMMAND}
