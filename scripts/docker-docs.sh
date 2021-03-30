68cc407fb0c39d978f9142582f98670c242dfbfc
vtog <v.tognaci@f5.com>
2021-03-30 12:43:03 -0500

    removed -i from docker for actions to work

--- scripts/docker-docs.sh
+++ scripts/docker-docs.sh
@@ -6,7 +6,7 @@ COMMAND=(/bin/bash -c "sudo pip install -U sphinx ; make -C docs html")
 
 : ${DOC_IMG:=f5devcentral/containthedocs:latest}
 
-exec docker run --rm -it \
+exec docker run --rm -t \
   -v $PWD:$PWD --workdir $PWD \
   ${DOCKER_RUN_ARGS} \
   -e "LOCAL_USER_ID=$(id -u)" \
