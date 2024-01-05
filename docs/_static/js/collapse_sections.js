7ab44e795be50a17ef661483c9549e7576250f24
Matt Mabis <m.mabis@f5.com>
2024-01-05 12:46:14 -0700

    ttest

---
+++ docs/_static/js/collapse_sections.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.toctree-wrapper .toctree').each(function() {
+        var $toctree = $(this);
+        var $toggleButton = $('<button type="button" class="btn btn-link toggle-button" data-toggle="collapse" data-target="#' + $toctree.attr('id') + '-collapse">&#9660;</button>');
+
+        $toctree.before($toggleButton);
+
+        $toggleButton.on('click', function() {
+            $toctree.toggleClass('collapsed');
+        });
+    });
+});
